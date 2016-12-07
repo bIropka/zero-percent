2228469cd38d1375def980794f465cb35b6a6d57
bIropka <biropka@gmail.com>
2016-12-07 12:22:13 +0200

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -13,6 +13,9 @@ $(document).ready(function () {
             $('.custom-select').removeClass('active');
             $('.custom-select').find('ul').stop().slideUp(200);
         }
+        if(!$(event.target).closest('.ui-rangeSlider-label-value').length && !$(event.target).closest('.ui-rangeSlider-label-inner').length) {
+            $('.ui-rangeSlider-label-value').removeClass('active');
+        }
     });
 
     setTimeout(function() {
@@ -152,13 +155,9 @@ $(document).ready(function () {
     $( "#min-cost" ).val( $("#filter-cost").rangeSlider('min') );
     $( "#max-cost" ).val( $("#filter-cost").rangeSlider('max') );
 
-});
-
     /******************************************************************************************************************
      ******* slider scripts
      ******************************************************************************************************************/
-
-
 
     $('.banner .slider').slick({
         appendArrows: '.banner-control',
@@ -180,12 +179,41 @@ $(document).ready(function () {
         step: 10
     });
 
-    $('.ui-rangeSlider-leftLabel .ui-rangeSlider-label-inner').html(defaultMin);
-    $('.ui-rangeSlider-rightLabel .ui-rangeSlider-label-inner').html(defaultMax);
+    $('<form action="#"><input type="text" /></form>').appendTo('.ui-rangeSlider-label-inner');
+    $('.ui-rangeSlider-label-inner input').css({
+        'display' : 'block',
+        'width': '100%',
+        'height': '100%',
+        'background': '#ffffff',
+        'color': '#000000',
+        'font-size': '0.8571428571428571em',
+        'font-family': '"PTSansBold", sans-serif',
+        'text-align': 'center',
+        'padding-top': '1px',
+        'border': 'none'
+    });
+
+    $('.ui-rangeSlider-leftLabel .ui-rangeSlider-label-inner input').attr('value', defaultMin);
+    $('.ui-rangeSlider-rightLabel .ui-rangeSlider-label-inner input').attr('value', defaultMax);
 
     $(".filter-slider").bind("valuesChanging", function(e, data){
-        $('.ui-rangeSlider-leftLabel .ui-rangeSlider-label-inner').html(data.values.min);
-        $('.ui-rangeSlider-rightLabel .ui-rangeSlider-label-inner').html(data.values.max);
+        var values = $(".filter-slider").rangeSlider("values");
+        $('.ui-rangeSlider-leftLabel .ui-rangeSlider-label-inner input').val(values.min);
+        $('.ui-rangeSlider-rightLabel .ui-rangeSlider-label-inner input').val(values.max);
     });
+
+    $('.ui-rangeSlider-label-value').click(function() {
+        $(this).toggleClass('active');
+    });
+
+    $('.ui-rangeSlider-label-inner form').submit(function(event) {
+        var min = $('.ui-rangeSlider-leftLabel .ui-rangeSlider-label-inner input').val();
+        var max = $('.ui-rangeSlider-rightLabel .ui-rangeSlider-label-inner input').val();
+        $(".filter-slider").rangeSlider("values", min, max);
+
+        event.preventDefault();
+    });
+
+});
 
 
